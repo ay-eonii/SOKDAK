53e821feec249082b3328e9a4f84dfe59165e7b2
ay-eonii <naayen@naver.com>
2025-05-23 01:22:27 +0900

    fix: 시간 변경

--- src/main/kotlin/me/chat/message/domain/entity/ChatMessage.kt
+++ src/main/kotlin/me/chat/message/domain/entity/ChatMessage.kt
@@ -3,6 +3,7 @@ package me.chat.message.domain.entity
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
+import java.time.ZoneId
 
 @Document(collection = "chat")
 data class ChatMessage(
@@ -10,5 +11,9 @@ data class ChatMessage(
     val id: String? = null,
     val content: String,
     val sender: String,
-    val timestamp: LocalDateTime = LocalDateTime.now()
-)
+    val timestamp: LocalDateTime = LocalDateTime.now(ASIA_SEOUL)
+) {
+    companion object {
+        private val ASIA_SEOUL: ZoneId = ZoneId.of("Asia/Seoul")
+    }
+}
