30d4304cbbc5a5cc3adb4755dcc61c03e283649d
ay-eonii <naayen@naver.com>
2025-06-02 16:58:46 +0900

    fix: 요청 로깅 제외 URI 설정

--- src/main/kotlin/me/chat/common/logging/CustomRequestLoggingFilter.kt
+++ src/main/kotlin/me/chat/common/logging/CustomRequestLoggingFilter.kt
@@ -13,12 +13,22 @@ class CustomRequestLoggingFilter : CommonsRequestLoggingFilter() {
         setAfterMessagePrefix("request = [")
     }
 
-    override fun shouldLog(request: HttpServletRequest): Boolean = true
+    override fun shouldLog(request: HttpServletRequest): Boolean {
+        return !EXCLUDE_URI_PREFIX.any { prefix -> request.requestURI.startsWith(prefix) }
+    }
 
     override fun beforeRequest(request: HttpServletRequest, message: String) {
     }
 
     override fun afterRequest(request: HttpServletRequest, message: String) {
         logger.info(message)
+    }
+
+    companion object {
+        private val EXCLUDE_URI_PREFIX: Set<String> = setOf(
+            "/js",
+            "/css",
+            "/favicon"
+        )
     }
 }
