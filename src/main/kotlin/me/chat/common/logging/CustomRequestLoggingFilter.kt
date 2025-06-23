8837767a2f5a6eb4115013a4757f3de4dce1f4f0
ay-eonii <naayen@naver.com>
2025-06-23 15:39:58 +0900

    fix: 로그 출력 형식 수정

--- src/main/kotlin/me/chat/common/logging/CustomRequestLoggingFilter.kt
+++ src/main/kotlin/me/chat/common/logging/CustomRequestLoggingFilter.kt
@@ -1,16 +1,18 @@
 package me.chat.common.logging
 
 import jakarta.servlet.http.HttpServletRequest
+import org.slf4j.LoggerFactory
 import org.springframework.web.filter.CommonsRequestLoggingFilter
 
 class CustomRequestLoggingFilter : CommonsRequestLoggingFilter() {
+    private val log = LoggerFactory.getLogger(CustomRequestLoggingFilter::class.java)
 
     init {
         isIncludeQueryString = true
         isIncludePayload = true
-        isIncludeClientInfo = true
         maxPayloadLength = 10000
-        setAfterMessagePrefix("request = [")
+        setAfterMessagePrefix("")
+        setAfterMessageSuffix("")
     }
 
     override fun shouldLog(request: HttpServletRequest): Boolean {
@@ -21,14 +23,16 @@ class CustomRequestLoggingFilter : CommonsRequestLoggingFilter() {
     }
 
     override fun afterRequest(request: HttpServletRequest, message: String) {
-        logger.info(message)
+        val user = request.userPrincipal?.name ?: "ANONYMOUS"
+        log.info("request = [{}, user={}]", message, user)
     }
 
     companion object {
         private val EXCLUDE_URI_PREFIX: Set<String> = setOf(
             "/js",
             "/css",
-            "/favicon"
+            "/favicon",
+            "/.well-known"
         )
     }
 }
